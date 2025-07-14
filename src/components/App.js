d46f5638f13ade1637c35782232ebb0d0f95ac26
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-14 13:05:58 +0300

    using API

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+
+import { response } from "msw";
+import { use, useEffect, useState } from "react";
+import { data } from "../mocks/data";
+
+export default function App() {
+    
+    const [image, setImage] = useState("")
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => setImage(data.message))
+    }, [])
+
+    if(!image) return <p>Loading...</p>
+
+    return (
+        <div>
+          <img src={image} alt="A Random Dog"/>
+        </div>
+    )
+}
